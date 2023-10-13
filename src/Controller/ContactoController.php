41cb367de1584371aaf913f7a3c2c02259babf9d
AndreuS07 <andreu2daw@gmail.com>
2023-10-13 10:44:08 +0200

    Operaciones

---
+++ src/Controller/ContactoController.php
@@ -0,0 +1,68 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ContactoController extends AbstractController{
+
+   
+    private $contactos = [
+        1 => ["nombre" => "Juan Pérez", "telefono" => "524142432", "email" => "juanp@ieselcaminas.org"],
+        2 => ["nombre" => "Ana López", "telefono" => "58958448", "email" => "anita@ieselcaminas.org"],
+        5 => ["nombre" => "Mario Montero", "telefono" => "5326824", "email" => "mario.mont@ieselcaminas.org"],
+        7 => ["nombre" => "Laura Martínez", "telefono" => "42898966", "email" => "lm2000@ieselcaminas.org"],
+        9 => ["nombre" => "Nora Jover", "telefono" => "54565859", "email" => "norajover@ieselcaminas.org"]
+
+    ];     
+
+
+    #[Route('/contacto/{codigo}', name:'ficha_contacto')] 
+public function ficha ($codigo): Response{
+
+    $resultado = ($this -> contactos[$codigo] ?? null);
+
+    return $this->render('ficha_contacto.html.twig', [
+        'contacto' => $resultado
+        ]);
+    }
+
+
+    #[Route('/contacto/buscar/{texto}', name:'ficha_buscar')] 
+public function buscar($texto): Response{
+
+    $resultados = array_filter($this->contactos, 
+        function ($contacto) use ($texto){
+            return strpos($contacto["nombre"], $texto) !== FALSE;
+
+        });
+
+        return $this->render('lista_contactos.html.twig', [
+            'contactos' => $resultados
+            ]);
+        }
+
+}
+
+use ApplEntityVCcontacto;
+use DoctrineVPersistenceWManagerRegistry;
+
+public function insertar (ManagerRegistry Sdoctrine){
+$entitymanager = $doctrine->getManager();
+foreach(Sthis->contactos as $c)f
+Scontacto = new Contacto();
+Scontacto->setNombre($c["nombre"]);
+$contacto->setTelefono($c["telefono"]);
+$contacto->setEmail(Sc["email"]);
+SentityManager ->persist ($contacto);
+}
+try
+{
+$entityManager->flush();
+
+return new Response("Contactos insertados");}
+catch (\Exception $e){}
+
+return new Response("Error insertando objetos");}}
